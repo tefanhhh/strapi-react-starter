bd2aa5ce061dceba7638a48bc1dafc8d2b030bb0
Tefan Haetami <tefanhaetami87@gmail.com>
2024-05-28 09:33:20 +0700

    chore: Add API module with Users model

---
+++ src/api/model.ts
@@ -0,0 +1,32 @@
+import instance from '@/api/instance'
+import { User } from '@/models/user.model'
+
+export class API<T> {
+  constructor(public resource: string) {}
+
+  async fetchAll() {
+    return await instance.get(`/${this.resource}`)
+  }
+
+  async fetchOne(id: string) {
+    return await instance.get(`/${this.resource}/${id}`)
+  }
+
+  async create(data: T  ) {
+    return await instance.post(`/${this.resource}`, data)
+  }
+
+  async update(id: string, data: T) {
+    return await instance.put(`/${this.resource}/${id}`, data)
+  }
+
+  async delete(id: string) {
+    return await instance.delete(`/${this.resource}/${id}`)
+  }
+}
+
+export class Users extends API<User> {
+  constructor() {
+    super('users')
+  }
+}
